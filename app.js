4c908a5ed10db61e37bca2d21942762f57d26c58
pollmix <polboy777@gmail.com>
2019-12-06 02:41:06 +0600

    Script complete

--- app.js
+++ app.js
@@ -1,55 +1,100 @@
-const fs = require('fs')
+const fs = require("fs");
 const $ = require("cheerio");
-const puppeteer = require('puppeteer');
-const slugify = require('slugify')
+const puppeteer = require("puppeteer");
+const slugify = require("slugify");
+const inquirer = require("inquirer");
 
-const url = "https://www.codewars.com/collections/python-8kyu-1";
+// const url = "https://www.codewars.com/collections/python-8kyu-1";
+// const folder_name = "8kyu";
+let url, folder_name;
 
-puppeteer.launch().then(async browser => {
-  const page = await browser.newPage();
-  await page.goto(url);
-  let content = await page.content();
-  let katas = [];
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "url",
+      message: "Codewars collection url?",
+      validate: function(value) {
+        if (value !== "") {
+          return true;
+        }
+        return "URL is required";
+      }
+    },
+    {
+      type: "input",
+      name: "folder_name",
+      message: "Folder name for problems?",
+      validate: function(value) {
+        if (value !== "") {
+          return true;
+        }
+        return "Folder name is required";
+      }
+    }
+  ])
+  .then(answers => {
+    console.log(answers);
+    url = answers.url;
+    folder_name = answers.folder_name;
 
-  $(".collection-items .item-title a", content).each(function () {
-    katas.push({
-      url: "https://www.codewars.com" + $(this).attr("href"),
-      title: $(this).text()
-    });
+    app(url, folder_name);
   });
 
-  for (let i = 0; i < 1; i++) {
-    const kata = katas[i];
-    await page.goto(kata.url);
-    await page.waitFor(() => document.querySelector('#description').innerText !== 'Loading description...')
+function app(url, folder_name) {
+  puppeteer.launch().then(async browser => {
+    const page = await browser.newPage();
+    await page.goto(url);
     let content = await page.content();
-    const description = $("#description", content);
-    kata.description = description.html()
-  }
+    let katas = [];
 
-  console.log(katas);
+    $(".collection-items .item-title a", content).each(function() {
+      katas.push({
+        url: "https://www.codewars.com" + $(this).attr("href"),
+        title: $(this).text()
+      });
+    });
 
-  let readme = `# codewars-solutions
-My collection of solutions for Codewars
-  
-## 8 Kyu`
+    for (let i = 0; i < katas.length; i++) {
+      const kata = katas[i];
+      await page.goto(kata.url);
+      await page.waitFor(
+        () =>
+          document.querySelector("#description").innerText !==
+          "Loading description..."
+      );
+      let content = await page.content();
+      const description = $("#description", content);
+      kata.description = description.html();
+    }
 
-  fs.writeFileSync('readme.md', readme)
+    console.log(katas);
 
-  katas.map((value, index) => {
-    let slug = slugify(value.title, { lower: true })
-    let template = `\n${index + 1}. [${value.title}](${value.url}) - [Solution](8kyu/${index + 1}-${slug}.md)`
-    fs.appendFileSync('readme.md', template)
+    let readme = `# codewars-solutions
+  My collection of solutions for Codewars
+    
+  ## 8 Kyu`;
 
+    fs.writeFileSync("readme.md", readme);
 
-    let problemTemplate = '### Problem:\n';
-    problemTemplate += value.description
-    problemTemplate += '\n### Solution'
+    if (!fs.existsSync(folder_name)) {
+      fs.mkdirSync(folder_name);
+    }
 
-    fs.writeFileSync(`8kyu/${index + 1}-${slug}.md`, problemTemplate)
+    katas.map((value, index) => {
+      let slug = slugify(value.title, { lower: true });
+      let template = `\n${index + 1}. [${value.title}](${
+        value.url
+      }) - [Solution](8kyu/${index + 1}-${slug}.md)`;
+      fs.appendFileSync("readme.md", template);
 
-  })
+      let problemTemplate = "### Problem:\n";
+      problemTemplate += value.description;
+      problemTemplate += "\n### Solution";
 
-  await browser.close();
-});
+      fs.writeFileSync(`8kyu/${index + 1}-${slug}.md`, problemTemplate);
+    });
 
+    await browser.close();
+  });
+}
