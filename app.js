c9a008337e45aad22e837b0da6b61d090db483af
pollmix <polboy777@gmail.com>
2019-12-06 02:05:16 +0600

    script updated

--- app.js
+++ app.js
@@ -1,48 +1,55 @@
-const axios = require("axios");
+const fs = require('fs')
 const $ = require("cheerio");
 const puppeteer = require('puppeteer');
-const kataInfo = require("./kata");
+const slugify = require('slugify')
 
 const url = "https://www.codewars.com/collections/python-8kyu-1";
 
-
 puppeteer.launch().then(async browser => {
   const page = await browser.newPage();
-  await page.goto('http://example.com/');
-  
-
-  page.once('load', () => console.log('Page loaded!----------------------------------------------'));
-  
-  const a = await page.content();
-
-  console.log(a);
-  
-  await browser.close();
-});
-
-
-const main = async () => {
-  const { data } = await axios.get(url);
+  await page.goto(url);
+  let content = await page.content();
   let katas = [];
 
-  $(".collection-items .item-title a", data).each(function() {
+  $(".collection-items .item-title a", content).each(function () {
     katas.push({
       url: "https://www.codewars.com" + $(this).attr("href"),
-      title: $(this).text(),
-      description: ''
+      title: $(this).text()
     });
   });
 
-
   for (let i = 0; i < 1; i++) {
     const kata = katas[i];
-
-    const result = await kataInfo(kata.url)
-
-    kata.description = result.description
+    await page.goto(kata.url);
+    await page.waitFor(() => document.querySelector('#description').innerText !== 'Loading description...')
+    let content = await page.content();
+    const description = $("#description", content);
+    kata.description = description.html()
   }
 
-  console.log(katas[0]);
-};
+  console.log(katas);
 
-// main()
+  let readme = `# codewars-solutions
+My collection of solutions for Codewars
+  
+## 8 Kyu`
+
+  fs.writeFileSync('readme.md', readme)
+
+  katas.map((value, index) => {
+    let slug = slugify(value.title, { lower: true })
+    let template = `\n${index + 1}. [${value.title}](${value.url}) - [Solution](8kyu/${index + 1}-${slug}.md)`
+    fs.appendFileSync('readme.md', template)
+
+
+    let problemTemplate = '### Problem:\n';
+    problemTemplate += value.description
+    problemTemplate += '\n### Solution'
+
+    fs.writeFileSync(`8kyu/${index + 1}-${slug}.md`, problemTemplate)
+
+  })
+
+  await browser.close();
+});
+
